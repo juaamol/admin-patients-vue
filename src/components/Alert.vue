37241981dc26b4ac859610bb33a0b63a61f9d5fc
juaamol <jambulud@gmail.com>
2023-06-21 11:52:01 +0200

    remove duplicated code from alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
   message: {
     type: String,
@@ -6,17 +8,15 @@ const props = defineProps({
   },
   type: { type: String, required: true },
 });
+
+const isError = computed(() => {
+  return props.type === 'error';
+});
 </script>
 <template>
   <div
-    v-if="type === 'success'"
-    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md bg-green-600"
-  >
-    {{ message }}
-  </div>
-  <div
-    v-if="type === 'error'"
-    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md bg-red-600"
+    :class="[isError ? 'bg-red-600' : 'bg-green-500']"
+    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
   >
     {{ message }}
   </div>
