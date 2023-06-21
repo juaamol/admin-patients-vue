bf11cf6736e8d1c4d9226c1bbbb9bf968a72bf29
juaamol <jambulud@gmail.com>
2023-06-21 22:26:16 +0200

    Create patient component

---
+++ src/components/Patient.vue
@@ -0,0 +1,74 @@
+<script setup>
+const props = defineProps({
+  petName: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  symptoms: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      ID:
+      <span class="font-normal normal-case"> </span>
+    </p>
+
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      Name:
+      <span class="font-normal normal-case">{{ petName }}</span>
+    </p>
+
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      Owner:
+      <span class="font-normal normal-case">{{ owner }} </span>
+    </p>
+
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      Email:
+      <span class="font-normal normal-case">{{ email }} </span>
+    </p>
+
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      Release date:
+      <span class="font-normal normal-case">{{ releaseDate }}</span>
+    </p>
+
+    <p class="font-bold mb-3 text-gray-700 uppercase">
+      Symptoms:
+      <span class="font-normal normal-case">{{ symptoms }} </span>
+    </p>
+
+    <div class="grid md:grid-cols-2 gap-5 mt-10">
+      <button
+        type="button"
+        class="block w-full py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+      >
+        Edit
+      </button>
+
+      <button
+        type="button"
+        class="block w-full py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
