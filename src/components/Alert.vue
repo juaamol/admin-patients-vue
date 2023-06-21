8c210d48460044f4222ccbdaf7f57a9cc62a286d
juaamol <jambulud@gmail.com>
2023-06-21 23:52:37 +0200

    Add transition opacity to alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -6,6 +6,7 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  show: Boolean,
   type: { type: String, required: true },
 });
 
@@ -15,8 +16,11 @@ const isError = computed(() => {
 </script>
 <template>
   <div
-    :class="[isError ? 'bg-red-600' : 'bg-green-500']"
-    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
+    :class="[
+      isError ? 'bg-red-600' : 'bg-green-500',
+      show ? 'opacity-100' : 'opacity-0',
+    ]"
+    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md transition-opacity duration-1000 fixed w-11/12 sm:w-9/12 md:w-6/12 m-auto left-0 right-0 bottom-4"
   >
     {{ message }}
   </div>
